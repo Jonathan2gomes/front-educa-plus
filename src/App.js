44d500ca58d340e8c2115516435615740b42c142
Jonathan <jonathan.gomes@govbr.com.br>
2021-06-21 23:30:04 -0300

    subindo projeto react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react";
+import api from "./api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    const [categoria, setCategoria] = React.useState("");
+    const [respostaCerta, setRespostaCerta] = React.useState("");
+    const [data, setData] = React.useState(null);
+    const [loading, setloading] = React.useState(null);
+
+    async function onSubmit(e) {
+        e.preventDefault();
+        setloading(true);
+        const response = await api.get(`?categoria=${categoria}`)
+        setData(response.data);
+        setloading(false);
+    }
+
+    const ValidarResposta = (respostaCerta) => {
+        if (respostaCerta) {
+            setRespostaCerta("Acertou, parabéns!!!");
+        } else {
+            setRespostaCerta("Tente novamente");
+        }
+    }
+    // function ValidarResposta(resposta) {
+    //     useEffect(() => {
+    //         if (resposta.respostaCerta) {
+    //             setRespostaCerta("Você acertou!");
+    //         }
+    //         setRespostaCerta("Você errou!");
+    //     }, [resposta])
+    // }
+
+    return (
+        <>
+            <div className='aplicativo' >
+
+                <input
+                    id="busca"
+                    type="search"
+                    value={categoria}
+                    placeholder="Digite a categoria"
+                    onChange={({target}) => setCategoria(target.value)}
+                />
+                <button type="submit" onClick={onSubmit} id="ok">
+                    Ok
+                </button>
+                {loading && (
+                    <p>carregando...</p>
+                )}
+                {!loading && data &&
+                <>
+                    <div className="perguntas" id="pergunta">{data.pergunta}</div>
+                    <ul className="respostas" id="respostas">{data.respostas.map((resposta) =>
+                        <li key={resposta.id}>
+                            <button
+                                id="resposta"
+                                onClick={() => ValidarResposta(resposta.respostaCerta)}>
+                                {resposta.descricaoResposta}
+                            </button>
+                        </li>
+                    )}</ul>
+                    <button onClick={onSubmit} id="proxima">Proxima</button>
+                    <div>{respostaCerta}</div>
+                </>
+                }
+            </div>
+        </>
+    );
+};
 
 export default App;
